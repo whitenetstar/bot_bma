87dfb627ccbd4dc09f7a6ec5748f5b60f5ab2d55
Athiwat <whitenetstar@gmail.com>
2019-01-09 14:21:05 +0700

    update bma_hook

--- bma_hooks.php
+++ bma_hooks.php
@@ -2,7 +2,7 @@
 
 require "vendor/autoload.php";
 require_once 'vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php';
-// require_once 'bot_library.php';
+require_once 'bot_library.php';
 
 $access_token = 'Vc4+AyNpHxJCvzDsINZQpYx0j89Y26XWpAefFLHqIsekth8Be35Z5JtScJfXONx2SIHHBnq2g1xORJVW93vJqLyebuuwyLQ3Wo5zAT7kjEdq+3JQtuhKlDSijQBG7tKeK2YdcsLG0Z8BUedjyZrkmAdB04t89/1O/w1cDnyilFU=';
 
